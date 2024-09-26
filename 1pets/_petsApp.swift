c354aaf7fa3b927586d39c5e94253fb429012a65
dallshaya <alshdeemah343@gmail.com>
2024-09-26 17:09:05 +0300

    Initial Commit

---
+++ 1pets/_petsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _petsApp.swift
+//  1pets
+//
+//  Created by deemah alshaya on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct _petsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
